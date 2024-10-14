50479774217888ffdcd57802e52d9945fc9ed653
DeshanHiripitiya <nipunadeshanhp@gmail.com>
2024-10-14 23:02:23 +0530

    commit

--- src/component/pages/Top.jsx
+++ src/component/pages/Top.jsx
@@ -19,7 +19,7 @@ function Top({ projectsRef, contactRef }) {
   return (
     <div
       id='top'
-      // className='bg-gradient-to-r from-[rgba(255,245,0,0.1)0%] via-[rgba(111,244,75,0.1)68%] to-[rgba(0,255,133,0.1)100%] pt-24 pb-12'
+      className='bg-gradient-to-r from-[rgba(255,245,0,0.1)0%] via-[rgba(111,244,75,0.1)68%] to-[rgba(0,255,133,0.1)100%] pt-24 pb-12'
     >
       <div className='flex flex-row items-center justify-center m-auto gap-16 p-8 lg:p-0'>
         <div>
