5a88b4e9900e42e46d2fe933b693cc55799ba58b
DeshanHiripitiya <nipunadeshanhp@gmail.com>
2024-10-19 23:56:46 +0530

    commit

--- src/component/pages/Skill.jsx
+++ src/component/pages/Skill.jsx
@@ -5,10 +5,12 @@ function Skill() {
   return (
     <div id='skills' className='py-12 mt-36'>
       <div className='flex flex-col items-center justify-center font-poppins'>
-        <p className='text-xl font-semibold text-[#0E4DA4] uppercase'>
+        <p className='text-base md:text-xl font-semibold text-[#0E4DA4] uppercase'>
           what i know
         </p>
-        <p className='text-4xl font-bold text-[#222222] mt-12'>My Skills</p>
+        <p className='text-2xl
+        md:text-3xl
+        lg:text-4xl font-bold text-[#222222] mt-12'>My Skills</p>
         <p className='mt-3 mx-auto max-w-3xl text-center !text-gray-500 '>
           <span className='italic'>
             "The more I learn, the more I realize how much I don't know. And
